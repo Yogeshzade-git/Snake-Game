acfc1e02c240f177e1249bd5719e617d0035d466
Yogesh Zade <90820986+Yogeshzade-git@users.noreply.github.com>
2024-04-06 00:35:48 +0530

    Add files via upload

---
+++ SnakeGame/src/snakegame/SnakeGame.java
@@ -0,0 +1,26 @@
+package snakegame;
+import javax.swing.*;
+
+/**
+ *
+ * @author Yogesh Zade
+ */
+public class SnakeGame extends JFrame {
+
+    SnakeGame(){
+        super("Snake Game");
+        add(new Board());
+        pack();
+        
+       
+        setLocationRelativeTo(null);
+        setResizable(false);
+        
+        
+    }
+    
+    public static void main(String[] args) {
+        new SnakeGame().setVisible(true);
+    }
+    
+}
